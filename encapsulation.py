fb15006501086d1edfca94357537a85740690a37
Madi <72963376+Madi-S@users.noreply.github.com>
2020-12-05 21:52:13 +0600

    encapsulation modified

--- encapsulation.py
+++ encapsulation.py
@@ -2,14 +2,14 @@
 
 class Car:
     def __init__(self, speed, colour):
-        self._speed = speed
-        self._colour = colour
+        self.__speed = speed
+        self.__colour = colour
 
     def displayInfo(self):
-        print(f'Colour: {self._colour}\nSpeed: {self._speed} km/h')
+        print(f'Colour: {self.__colour}\nSpeed: {self.__speed} km/h')
 
 
 c = Car(100, 'gray')
 c.displayInfo()
 
-print(c._speed, c._colour)
+# print(c.__speed, c.__colour) - will raise an AttirbuteError `'Car' object has no attribute '__speed'`
